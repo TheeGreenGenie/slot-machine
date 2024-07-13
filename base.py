4955ab5404016165d66e7278035cefdb584860b5
theegreengenie <solomonshasanmi@gmail.com>
2024-07-12 21:15:52 -0400

    bet and line functions

--- base.py
+++ base.py
@@ -1,14 +1,54 @@
+MAX_LINES = 3
+MAX_BET = 100
+MIN_BET = 1
+
 
 def deposit():
     while True:
-        amount = input("What would you like to deposit? $")
+        amount = input("What would you like to deposit? $ ")
         if amount.isdigit():
             amount = int(amount)
             if amount > 0:
                 break
             else:
-                print('Amount must be greater than 0')
+                print('Amount must be greater than 0.')
         else:
-            print('Please enter a number')
+            print('Please enter a number.')
 
     return amount
+
+def get_num_of_lines():
+    while True:
+        lines = input(f"Enter the number of lines you want to bet on (1-{MAX_LINES})? ")
+        if lines.isdigit():
+            lines = int(lines)
+            if lines in range(1,4):
+                break
+            else:
+                print('Enter a valid number of lines.')
+        else:
+            print('Please enter a number.')
+
+    return lines
+
+def get_bet():
+    while True:
+        bet = input(f"What would you like to bet on each line? ")
+        if bet.isdigit():
+            bet = int(bet)
+            if bet in range(MIN_BET,MAX_BET+1):
+                break
+            else:
+                print(f'Amount must be between ${MIN_BET} & ${MAX_BET}.')
+        else:
+            print('Please enter a number.')
+    return bet
+
+def main():
+    balance = deposit()
+    lines = get_num_of_lines()
+    bet = get_bet()
+    print(balance, lines)
+
+
+main()
